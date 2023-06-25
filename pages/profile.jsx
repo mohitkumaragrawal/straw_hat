e470fc25831ffd642134cd97d1aac056a2d73034
Aditya5510 <78204626+Aditya5510@users.noreply.github.com>
2023-06-25 12:41:32 +0530

    profile changed

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,12 +1,9 @@
 import React, { useEffect, useState } from 'react';
-import { Row, Col, Container } from 'reactstrap';
 import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
-
 import Loading from '../components/Loading';
 import ErrorMessage from '../components/ErrorMessage';
-import Highlight from '../components/Highlight';
+
 import { Avatar, Box, Typography } from '@mui/material';
-import Ntable from '../components/Ntable';
 
 import axios from 'axios';
 
@@ -30,36 +27,34 @@ function Profile() {
   }, []);
 
   return (
-    <Container>
-      <Box
-        sx={{
-          mt: '10px',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          gap: '10 rem',
-          flexDirection: 'column'
-        }}>
-        {isLoading && <Loading />}
-        {user && (
-          <>
-            <Box
-              sx={{
-                mt: '10px',
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                gap: '1 rem',
-                flexDirection: 'column'
-              }}>
-              <Avatar src={user.picture} sx={{ width: '100px', height: '100px' }} />
-              <Typography variant="h4">{user.name}</Typography>
-            </Box>
-            <Ntable />
-          </>
-        )}
-      </Box>
-    </Container>
+    <Box
+      sx={{
+        mt: '10px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        gap: '10 rem',
+        flexDirection: 'column'
+      }}>
+      {isLoading && <Loading />}
+      {user && (
+        <Box>
+          <Box
+            sx={{
+              mt: '10px',
+              display: 'flex',
+              alignItems: 'center',
+              justifyContent: 'center',
+              gap: '1 rem',
+              flexDirection: 'column'
+            }}>
+            <Avatar src={user.picture} sx={{ width: '100px', height: '100px', mb: '3rem' }} />
+            <Typography variant="h4">{user.name}</Typography>
+            <hr />
+          </Box>
+        </Box>
+      )}
+    </Box>
   );
 }
 
