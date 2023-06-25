c8d6c5c7b4acab29ab030b0c73cb4e36835f7528
Aditya5510 <78204626+Aditya5510@users.noreply.github.com>
2023-06-25 12:10:40 +0530

    profile 1

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -5,7 +5,8 @@ import { useUser, withPageAuthRequired } from '@auth0/nextjs-auth0/client';
 import Loading from '../components/Loading';
 import ErrorMessage from '../components/ErrorMessage';
 import Highlight from '../components/Highlight';
-import { Box } from '@mui/material';
+import { Avatar, Box, Typography } from '@mui/material';
+import Ntable from '../components/Ntable';
 
 import axios from 'axios';
 
@@ -30,30 +31,31 @@ function Profile() {
 
   return (
     <Container>
-      <Box sx={{ mt: '10px' }}>
+      <Box
+        sx={{
+          mt: '10px',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '10 rem',
+          flexDirection: 'column'
+        }}>
         {isLoading && <Loading />}
         {user && (
           <>
-            <Row className="align-items-center profile-header mb-5 text-center text-md-left" data-testid="profile">
-              <Col md={2}>
-                <img
-                  src={user.picture}
-                  alt="Profile"
-                  className="rounded-circle img-fluid profile-picture mb-3 mb-md-0"
-                  decode="async"
-                  data-testid="profile-picture"
-                />
-              </Col>
-              <Col md>
-                <h2 data-testid="profile-name">{user.name}</h2>
-                <p className="lead text-muted" data-testid="profile-email">
-                  {user.email}
-                </p>
-              </Col>
-            </Row>
-            {/* <Row data-testid="profile-json">
-            <Highlight>{JSON.stringify(user, null, 2)}</Highlight>
-          </Row> */}
+            <Box
+              sx={{
+                mt: '10px',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                gap: '1 rem',
+                flexDirection: 'column'
+              }}>
+              <Avatar src={user.picture} sx={{ width: '100px', height: '100px' }} />
+              <Typography variant="h4">{user.name}</Typography>
+            </Box>
+            <Ntable />
           </>
         )}
       </Box>
